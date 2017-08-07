184547660fcde98ac01d7001df07ae56fc8cc3c8
j-k-night <jessica.knight@bellevuecollege.edu>
2017-08-06 20:25:36 -0700

    Update function.js

--- final_project/function.js
+++ final_project/function.js
@@ -1,3 +1,3 @@
 var foot = document.getElementsByClassName('foot');
-var checkers = '<p><a href="http://validator.w3.org" target="_blank"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="HTML Checker" height="41" width="88" /></a><a href="http://jigsaw.w3.org/css-validator" target="_blank"><img style="border:0;width:88px;height:41px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Checker" /></a></p>';
+var checkers = '<p><a href="http://validator.w3.org" target="_blank"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="HTML Checker" class = "checker_image"/></a><a href="http://jigsaw.w3.org/css-validator" target="_blank"><img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Checker"  class = "checker_image"/></a></p>';
 foot[0].innerHTML = Date() + checkers;
