2bc1250ed8d8881ff575d94fb7f585ccd2c3f807
j-k-night <jessica.knight@bellevuecollege.edu>
2017-08-06 19:40:24 -0700

    Update function.js

--- final_project/function.js
+++ final_project/function.js
@@ -1,3 +1,3 @@
 var foot = document.getElementsByClassName('foot');
-var msg = "<p>I am the footer.</p>";
-foot[0].innerHTML = Date();
+var checkers = '<p><a href="http://validator.w3.org/check?uri=referer" target="_blank"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0" height="41" width="88" /></a><a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank"><img style="border:0;width:88px;height:41px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /></a></p>';
+foot[0].innerHTML = Date() + checkers;
